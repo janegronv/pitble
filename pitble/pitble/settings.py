c6f960cd8c3add5275b78fc851de33e6e8e4407f
Jose A <janegronv@gmail.com>
2015-08-14 13:10:56 +0200

    paso 1

--- pitble/pitble/settings.py
+++ pitble/pitble/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pitble.pitapp'
+    'pitble.pitapp',
 )
 
 MIDDLEWARE_CLASSES = (
