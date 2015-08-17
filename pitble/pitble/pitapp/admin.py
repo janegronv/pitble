6eac354cba94ff5bbb273f19a7d2d89257d9ca3b
Jose A <janegronv@gmail.com>
2015-08-17 14:17:51 +0200

    .

--- pitble/pitble/pitapp/admin.py
+++ pitble/pitble/pitapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
 from pitble.pitapp.models import Pitble
 
 # Register your models here.
@@ -6,4 +9,5 @@ class PitbleAdmin(admin.ModelAdmin):
      list_display = ('text','owner')
      search_fields = ('text',)
 
+admin.site.register(get_user_model(), UserAdmin)
 admin.site.register(Pitble, PitbleAdmin)
