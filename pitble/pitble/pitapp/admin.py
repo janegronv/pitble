19d1b85131dcc4b70959525ac4251ce6ff0f218a
Jose A <janegronv@gmail.com>
2015-08-17 13:53:57 +0200

    añadiendo un campo nuevo al formulario de Users

--- pitble/pitble/pitapp/admin.py
+++ pitble/pitble/pitapp/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+
 from pitble.pitapp.models import Pitble
 
 # Register your models here.
+
+class PitbleUserAdmin(UserAdmin):
+     fieldsets = (
+     (None, {'fields': ('username', 'password')}),
+     (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+     (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+     'groups', 'user_permissions')}),
+     (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+     (_('Pitble fields'),{'fields': ('followings',)}),
+     )
+
 class PitbleAdmin(admin.ModelAdmin):
      list_display = ('text','owner')
      search_fields = ('text',)
 
+admin.site.register(get_user_model(), PitbleUserAdmin)
 admin.site.register(Pitble, PitbleAdmin)
